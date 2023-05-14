2eaa3b933ced8ff0a85150512d255f88e05644cb
Stanko Uzunov <stanko.uzunov@broadcom.com>
2023-05-14 16:47:33 +0200

    Add function to count slides in slider

--- src/main/java/automation/HomePage.java
+++ src/main/java/automation/HomePage.java
@@ -1,13 +1,19 @@
 package automation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class HomePage extends BasePage {
     @FindBy(xpath = "\"//*[@id=\\\"menu-item-50\\\"]/a\"")
     WebElement myAccount;
+    @FindBy(xpath = "//*[@id=\"n2-ss-6\"]/div[1]/div/div")
+    WebElement slider;
+    By slidesXPaths = By.xpath("./child::*");
     WebDriver webDriver;
     String targetUrl;
     public HomePage(WebDriver webDriver, String targetUrl) {
@@ -28,5 +34,8 @@ public class HomePage extends BasePage {
         return myAccExists;
     }
 
-    //public int findNumberOfSlides() { return slidesCount; }
+    public int findNumberOfSlides() {
+        List<WebElement> slides = slider.findElements(slidesXPaths);
+        return slides.size();
+    }
 }
